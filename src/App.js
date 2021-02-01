b26bc5a8c6bbf2ad1595f55d52f1e488ffa766eb
Thomas Guo <gameking93@gmail.com>
2021-02-01 17:32:41 +0800

    chore: initdevelop env

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+export class App extends PureComponent{
+
+    render(){
+        return(
+          <div className="App">
+            <p>開發環境建立</p>
+          </div>
+        );
+    }
+}
+
+export default App
